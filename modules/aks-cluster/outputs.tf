3033234d564bed81efebd6c2ef4321b8f9fa035e
Alejandro Serrano Ortega <aserrano@futurespace.es>
2026-04-10 13:20:20 +0200

    feat(aks-cluster): add oidc issuer output

--- modules/aks-cluster/outputs.tf
+++ modules/aks-cluster/outputs.tf
@@ -18,3 +18,6 @@ output "kube_config" {
   value     = azurerm_kubernetes_cluster.this.kube_config_raw
   sensitive = true
 }
+output "oidc_issuer_url" {
+  value = azurerm_kubernetes_cluster.this.oidc_issuer_url
+}
